8e5805913c3cf26b9572e709e752b7cd3cbea9c0
casspc <casspc@gmail.com>
2014-06-09 14:15:59 -0700

    moving files in from demo project

---
+++ src/main/java/com/krux/beacon/listener/BeaconListenerServer.java
@@ -0,0 +1,38 @@
+package com.krux.beacon.listener;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Simplistic telnet server.
+ */
+public class BeaconListenerServer {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8992" : "8023"));
+
+    public static void main(String[] args) throws Exception {
+        
+        //krux std lib stuff
+        
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new BeaconListenerInitializer());
+
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
