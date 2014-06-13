4d462c8fb3f1e9c836b807e6507b25305542d6e8
casspc <casspc@gmail.com>
2014-06-13 13:24:18 -0700

    mo betta cl options

--- src/main/java/com/krux/beacon/listener/kafka/producer/KafkaProducer.java
+++ src/main/java/com/krux/beacon/listener/kafka/producer/KafkaProducer.java
@@ -22,6 +22,17 @@ public class KafkaProducer {
                 System.getProperty("partitioner.class", "com.krux.beacon.listener.kafka.producer.SimplePartitioner"));
         props.put("request.required.acks", System.getProperty("request.required.acks", "1"));
         props.put("producer.type", System.getProperty("producer.type", "async"));
+        
+        props.put("request.timeout.ms", System.getProperty("request.timeout.ms", "10000"));
+        props.put("compression.codec", System.getProperty("compression.codec", "none"));
+        props.put("message.send.max.retries", System.getProperty("message.send.max.retries", "3"));
+        props.put("retry.backoff.ms", System.getProperty("retry.backoff.ms", "100"));
+        props.put("queue.buffering.max.ms", System.getProperty("queue.buffering.max.ms", "5000" ));
+        props.put("queue.buffering.max.messages", System.getProperty("queue.buffering.max.messages", "10000"));
+        props.put("queue.enqueue.timeout.ms", System.getProperty("queue.enqueue.timeout.ms", "-1"));
+        props.put("batch.num.messages", System.getProperty("batch.num.messages", "200"));
+        props.put("client.id", System.getProperty("client.id", ""));
+        props.put("send.buffer.bytes", System.getProperty("send.buffer.bytes", String.valueOf( 100 * 1024 )));
 
         ProducerConfig config = new ProducerConfig(props);
         producer = new Producer<String, String>(config);
