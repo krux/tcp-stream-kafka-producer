7dfe10668e0899a4d4ccd6b783ae8b95c9e75b95
casspc <casspc@gmail.com>
2014-08-14 15:51:30 -0700

    some new metrics

--- src/main/java/com/krux/beacon/listener/BeaconListenerHandler.java
+++ src/main/java/com/krux/beacon/listener/BeaconListenerHandler.java
@@ -1,5 +1,6 @@
 package com.krux.beacon.listener;
 
+import static com.codahale.metrics.MetricRegistry.*;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -8,11 +9,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
 import com.krux.beacon.listener.kafka.producer.KafkaProducer;
 import com.krux.server.http.StdHttpServerHandler;
 import com.krux.stdlib.KruxStdLib;
@@ -26,21 +29,35 @@ public class BeaconListenerHandler extends SimpleChannelInboundHandler<String> {
     private static final Logger LOG = LoggerFactory.getLogger(BeaconListenerHandler.class.getName());
     private static final Map<String,Long> lastTopicTimes = Collections.synchronizedMap(
             new HashMap<String,Long>());
+    private static final Map<String,Object> rps = Collections.synchronizedMap(
+            new HashMap<String,Object>());
+    private static Map<String,Meter> rqsMeters = null;
+    
+    private static final MetricRegistry metrics = new MetricRegistry();
     
     static {    
-        StdHttpServerHandler.addAdditionalStatus( "topicProcessingTimesMs", lastTopicTimes );
+        StdHttpServerHandler.addAdditionalStatus( "latestTopicProcessingTimesNanoSec", lastTopicTimes );
+        StdHttpServerHandler.addAdditionalStatus( "topicQps", rps );
     }
 
     private List<String> _topics;
 
     public BeaconListenerHandler(List<String> topics) {
         _topics = topics;
+        
+        if ( rqsMeters == null ) {
+            rqsMeters = Collections.synchronizedMap( new HashMap<String,Meter>() );
+            for ( String topic : _topics ) {
+                rqsMeters.put( topic, metrics.meter(name(BeaconListenerHandler.class, topic + "_requests")));
+            }
+        }
     }
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, String request) throws Exception {
         
         long start = System.currentTimeMillis();
+        long startNs = System.nanoTime();
 
         // All we do here is take the incoming message and plop it onto the
         // configured kafka topic(s). Too easy
@@ -51,8 +68,20 @@ public class BeaconListenerHandler extends SimpleChannelInboundHandler<String> {
             try {
                 KafkaProducer.send(topic, request);
                 long time = System.currentTimeMillis() - start;
-                lastTopicTimes.put( topic, time );
+                long timeNs = System.nanoTime() - startNs;
+                lastTopicTimes.put( topic, timeNs );
                 KruxStdLib.STATSD.time("message_processed_" + topic, time);
+                
+                rqsMeters.get( topic ).mark();
+                Map<String,Object> qpsMap = new HashMap<String,Object>();
+                Meter m = rqsMeters.get( topic );
+                qpsMap.put( "count", m.getCount() );
+                qpsMap.put( "1_min_rate", m.getOneMinuteRate() );
+                qpsMap.put( "5_min_rate", m.getFiveMinuteRate() );
+                qpsMap.put( "15_min_rate", m.getFifteenMinuteRate() );
+                qpsMap.put( "mean_rate", m.getMeanRate() );
+                
+                rps.put( topic, qpsMap );
             } catch ( Exception e ) {
                 long time = System.currentTimeMillis() - start;
                 KruxStdLib.STATSD.time("message_error_" + topic, time);                
