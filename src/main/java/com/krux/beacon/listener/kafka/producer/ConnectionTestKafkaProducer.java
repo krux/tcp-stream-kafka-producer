716c85221ec85127050c6efc0611e4bdc2881e99
Prashanth Jonnalagadda <prashanth@krux.com>
2016-06-08 13:22:46 -0700

    Fix to set metadata broker list for test producer

--- src/main/java/com/krux/beacon/listener/kafka/producer/ConnectionTestKafkaProducer.java
+++ src/main/java/com/krux/beacon/listener/kafka/producer/ConnectionTestKafkaProducer.java
@@ -1,5 +1,7 @@
 package com.krux.beacon.listener.kafka.producer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -17,26 +19,31 @@ public class ConnectionTestKafkaProducer {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionTestKafkaProducer.class.getName());
 
+    private static String HOSTNAME;
+
     static {
 
         Properties props = new Properties();
 
-        // all the below properties are set in BeaconListenerServer after
-        // parsing the command line options
+        // metadata broker list is set in TCPStreamListenerServer
         props.put("metadata.broker.list", System.getProperty("metadata.broker.list", "localhost:9092"));
-        props.put("serializer.class", System.getProperty("serializer.class", "kafka.serializer.StringEncoder"));
-        props.put("partitioner.class", System.getProperty("partitioner.class", "com.krux.kafka.producer.SimplePartitioner"));
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "com.krux.kafka.producer.SimplePartitioner");
         props.put("producer.type", "sync");
         props.put("message.send.max.retries", "0");
 
         ProducerConfig config = new ProducerConfig(props);
         PRODUCER = new Producer<String, String>(config);
-
+        try {
+            HOSTNAME = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            HOSTNAME = "UnknownHost";
+        }
     }
 
     public static void sendTest(String topic) {
         long start = System.currentTimeMillis();
-        KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, "", "This is a test");
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, "", "This is a test - " + HOSTNAME);
         LOG.debug("Sending test message to {}", topic);
         PRODUCER.send(data);
         LOG.debug("SENT test message to {}", topic);
